266c982c6c0ace2304e8aa59f9574cfbfa4f55bb
Martin <martin_lenart@icloud.com>
2023-10-26 18:07:58 +0200

    update

---
+++ M3_09_05/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace M3_09_05;
+
+class Program
+{
+    public enum RectColor
+    {
+        red, blue, yellow, white, pink
+    }
+    public class Rectangle : IEquatable<Rectangle>
+    {
+        public RectColor Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Area => Height * Width;
+
+        public bool Equals(Rectangle other) => (Height, Width, Color) == (other.Height, other.Width, other.Color);
+        public override int GetHashCode() => (Width, Height, Color).GetHashCode();  //Needed to implement as part of IEquatable
+        public override bool Equals(object obj) => Equals(obj as Rectangle); //Needed to implement as part of IEquatable
+
+        public override string ToString() => $"{Color} rectangle with height: {Height:N2}, width: {Width:N2} and area: {Area:N2}";
+    }
+
+    static void Main(string[] args)
+    {
+        //HashSet<T>
+        HashSet<RectColor> hs1 = new HashSet<RectColor>();
+        hs1.Add(RectColor.red);
+        hs1.Add(RectColor.yellow);
+        hs1.Add(RectColor.red);
+        hs1.Add(RectColor.blue);
+        hs1.Add(RectColor.white);
+        hs1.Add(RectColor.pink);
+        hs1.Add(RectColor.red);
+        hs1.Add(RectColor.white);
+
+        //Convert to list and write out. Only unique RectColors
+        foreach (var item in hs1.ToList())
+        {
+            Console.WriteLine(item);
+        }
+
+        //HashSet use IEqutable and GetHashCode(), so any object implementing IEqutable and GetHashCode() can be used
+        HashSet<Rectangle> hs2 = new HashSet<Rectangle>() {
+                new Rectangle(){ Color = RectColor.pink, Height = 45, Width =15 },
+                new Rectangle(){ Color = RectColor.red, Height = 10, Width = 20 },
+                new Rectangle(){ Color = RectColor.pink, Height = 45, Width =15 },
+                new Rectangle(){ Color = RectColor.red, Height = 10, Width = 20 },
+                new Rectangle(){ Color = RectColor.blue, Height = 10, Width = 20 }};
+
+        //Convert to list and write out. Only unique Rectangles
+        Console.WriteLine();
+        foreach (var item in hs2.ToList())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
---
+++ M3_09_06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace M3_09_06;
+
+class Program
+{
+    public enum RectColor
+    {
+        red, blue, yellow, white, pink
+    }
+
+    static void Main(string[] args)
+    {
+        //Use dictionary to map a List of somthing to a key
+        Dictionary<string, List<string>> FavoriteBands = new Dictionary<string, List<string>>();
+
+        FavoriteBands.Add("ACDC", new List<string>() { "Fly on the Wall", "TnT", "For those about to Rock" });
+        FavoriteBands.Add("PinkFloyd", new List<string>() { "Dark side of the moon", "The Wall", "Final Cut" });
+        FavoriteBands.Add("Bob Dylan", new List<string>() { "Infidels" });
+
+        Console.WriteLine(FavoriteBands.ContainsKey("Abba"));
+        foreach (var performer in FavoriteBands.Keys)
+        {
+            Console.WriteLine($"\n{performer} albums:");
+            foreach (var album in FavoriteBands[performer])
+            {
+                Console.WriteLine(album);
+            }
+        }
+
+        //Use Dictionary to map a key to the nr occurances in a list
+        //HashSet<T>
+        List<RectColor> list = new List<RectColor>() { RectColor.red, RectColor.yellow, RectColor.red, RectColor.blue,
+            RectColor.white, RectColor.pink, RectColor.red, RectColor.white };
+
+        //Check number of occurnaces in the list
+        Dictionary<RectColor, int> occurances = new Dictionary<RectColor, int>();
+        foreach (var item in list)
+        {
+            //try to see if it can be added, only the first time works
+            if (!occurances.TryAdd(item, 1))
+            {
+                //already exists, simply imcrement the value
+                occurances[item]++;
+            }
+        }
+
+        //print out the number of occurances
+        Console.WriteLine();
+        foreach (var item in occurances.Keys)
+        {
+            Console.WriteLine($"{item}: {occurances[item]} times");
+        }
+    }
+}
+
